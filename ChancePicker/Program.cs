b33c87846b1773f1aa9448f4768e1716bb05a271
Bogdan Tabusca <btabusca@endava.com>
2019-05-06 10:46:33 +0300

    Add project files.

---
+++ ChancePicker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChancePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
