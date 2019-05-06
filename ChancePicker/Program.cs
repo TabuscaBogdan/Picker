510373339219cf26a56f7d33a99dbf80c32bb8af
Bogdan Tabusca <btabusca@endava.com>
2019-05-06 16:42:05 +0300

    tested

--- ChancePicker/Program.cs
+++ ChancePicker/Program.cs
@@ -6,7 +6,22 @@ namespace ChancePicker
     {
         static void Main(string[] args)
         {
-            
+            var picker = new WeightedPicker();
+            var ch1 = new Choice {Name = "BigSpender", Points = 10};
+            var ch2 = new Choice {Name = "SmallFry", Points = 1};
+            var ch3 = new Choice {Name = "Investor1", Points = 2};
+            var ch4 = new Choice {Name = "Investor2", Points = 3};
+            var ch5 = new Choice {Name = "RiskTaker", Points = 2};
+            var ch6 = new Choice {Name = "SmallEnterprize", Points = 2};
+
+            picker.AddChoice(ch1);
+            picker.AddChoice(ch2);
+            picker.AddChoice(ch3);
+            picker.AddChoice(ch4);
+            picker.AddChoice(ch5);
+            picker.AddChoice(ch6);
+
+            picker.ShowStatus();
         }
     }
 }
